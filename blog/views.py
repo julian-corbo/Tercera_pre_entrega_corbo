5f4f9b18ffb6b2cea7160e8a2e4fbff5b82e3981
Julian C <juliancorbo00@gmail.com>
2023-11-07 12:50:25 -0300

    Views de listado Creadas

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,46 @@
 from django.shortcuts import render
 
-from blog.models import Cafeterias
+from blog.models import Cafeterias,Recetas,Articulos
+
+def lista_cafeterias(request):
+
+    contexto = {
+        'cafeterias':Cafeterias.objects.all()
+    }
+
+    http_response = render(
+        request=request,
+        template_name='blog/cafeterias.html',
+        context=contexto,
+    )
+    
+    return http_response
 
 
+def lista_recetas(request):
 
+    contexto = {
+        'recetas':Recetas.objects.all()
+    }
 
+    http_response = render(
+        request=request,
+        template_name='blog/recetas.html',
+        context=contexto,
+    )
+    
+    return http_response
+
+def lista_articulos(request):
+
+    contexto = {
+        'articulos':Articulos.objects.all()
+    }
+
+    http_response = render(
+        request=request,
+        template_name='blog/articulos.html',
+        context=contexto,
+    )
+    
+    return http_response
