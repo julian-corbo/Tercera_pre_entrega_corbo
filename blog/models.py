b8c1e2aaa65ada67ca42577600dc3554a655e3e4
Julian C <juliancorbo00@gmail.com>
2023-11-07 12:03:21 -0300

    Modelos creados

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+class Cafeterias(models.Model):
+    nombre = models.CharField(max_length=64)
+    direccion = models.CharField(max_length=1024)
+    
+class Recetas(models.Model):
+    nombre = models.CharField(max_length=64)
+    receta = models.TextField()
+
+class Articulos(models.Models):
+    autor = models.CharField(max_length=64)
+    cafeteria_reseniada = models.ForeignKey(Cafeterias, on_delete=models.CASCADE)
+    
+    titulo = models.CharField(max_length=64)
+    texto = models.TextField()
+    puntaje = models.IntegerField(
+        default=0, validators=[MinValueValidator(1), MaxValueValidator(5)]
+    )
