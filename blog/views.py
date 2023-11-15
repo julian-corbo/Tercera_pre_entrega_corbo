d3b97fc0966399f21092de6932af8faa26e5d404
Julian C <juliancorbo00@gmail.com>
2023-11-15 16:22:47 -0300

    Busqueda funcional

--- blog/views.py
+++ blog/views.py
@@ -100,7 +100,7 @@ class RecetasDeleteView(LoginRequiredMixin,DeleteView):
    model = Recetas
    success_url = reverse_lazy('lista_recetas')
 
-#Vista Funcional
+
 #Buscar
 def buscar_cafeteria(request):
    if request.method == "POST":
@@ -108,7 +108,7 @@ def buscar_cafeteria(request):
        busqueda = data["busqueda"]
        cafeteria = Cafeterias.objects.filter(nombre__icontains=busqueda)
        contexto = {
-           "cafeterias": cafeteria,
+           "object_list": cafeteria,
        }
        http_response = render(
            request=request,
@@ -123,7 +123,7 @@ def buscar_receta(request):
        busqueda = data["busqueda"]
        receta = Recetas.objects.filter(nombre__icontains=busqueda)
        contexto = {
-           "recetas": receta,
+           "object_list": receta,
        }
        http_response = render(
            request=request,
@@ -140,7 +140,7 @@ def buscar_articulo(request):
            Q(autor__icontains= busqueda) | Q(cafeteria_reseniada__nombre__icontains= busqueda) | Q(titulo__icontains= busqueda)    
        )
        contexto = {
-           "articulos": articulo,
+           "object_list": articulo,
        }
        http_response = render(
            request=request,
@@ -149,126 +149,6 @@ def buscar_articulo(request):
        )
        return http_response
 
-
-#listView
-def lista_cafeterias(request):
-
-    contexto = {
-        'cafeterias':Cafeterias.objects.all()
-    }
-
-    http_response = render(
-        request=request,
-        template_name='blog/cafeterias.html',
-        context=contexto,
-    )
-    
-    return http_response
-
-def lista_recetas(request):
-
-    contexto = {
-        'recetas':Recetas.objects.all()
-    }
-
-    http_response = render(
-        request=request,
-        template_name='blog/recetas.html',
-        context=contexto,
-    )
-    
-    return http_response
-
-def lista_articulos(request):
-
-    contexto = {
-        'articulos':Articulos.objects.all()
-    }
-
-    http_response = render(
-        request=request,
-        template_name='blog/articulos.html',
-        context=contexto,
-    )
-    
-    return http_response
-
-#create view
-def crear_cafeteria(request):
-   if request.method == "POST":
-       formulario = CafeteriaFormulario(request.POST)
-
-       if formulario.is_valid():
-           data = formulario.cleaned_data  # es un diccionario
-           nombre = data["nombre"]
-           direccion = data["direccion"]
-           cafeteria = Cafeterias(nombre=nombre, direccion=direccion)  # lo crean solo en RAM
-           cafeteria.save()  # Lo guardan en la Base de datos
-
-           # Redirecciono al usuario a la lista 
-           url_exitosa = reverse('lista_cafeterias')  
-           return redirect(url_exitosa)
-   else:  # GET
-       formulario = CafeteriaFormulario()
-
-   http_response = render(
-       request=request,
-       template_name='blog/formulario_cafeterias.html',
-       context={'formulario': formulario},
-   )
-   return http_response
-
-def crear_articulo(request):
-   if request.method == "POST":
-       formulario = ArticulosFormulario(request.POST)
-
-       if formulario.is_valid():
-           data = formulario.cleaned_data  # es un diccionario
-           autor = data["autor"]
-           cafeteria_reseniada = data["cafeteria_reseniada"]
-           titulo = data["titulo"]
-           texto = data["texto"]
-           puntaje = data["puntaje"]
-
-           articulo = Articulos(autor=autor, cafeteria_reseniada=cafeteria_reseniada, titulo=titulo, texto=texto, puntaje=puntaje)  # lo crean solo en RAM
-           articulo.save()  # Lo guardan en la Base de datos
-
-           # Redirecciono al usuario a la lista 
-           url_exitosa = reverse('lista_articulos')  
-           return redirect(url_exitosa)
-   else:  # GET
-       formulario = ArticulosFormulario()
-
-   http_response = render(
-       request=request,
-       template_name='blog/formulario_articulos.html',
-       context={'formulario': formulario},
-   )
-   return http_response
-
-def crear_receta(request):
-   if request.method == "POST":
-       formulario = RecetasFormulario(request.POST)
-
-       if formulario.is_valid():
-           data = formulario.cleaned_data  # es un diccionario
-           nombre = data["nombre"]
-           receta = data["receta"]
-           receta = Recetas(nombre=nombre, receta=receta)  # lo crean solo en RAM
-           receta.save()  # Lo guardan en la Base de datos
-
-           # Redirecciono al usuario a la lista 
-           url_exitosa = reverse('lista_recetas')  
-           return redirect(url_exitosa)
-   else:  # GET
-       formulario = RecetasFormulario()
-
-   http_response = render(
-       request=request,
-       template_name='blog/formulario_recetas.html',
-       context={'formulario': formulario},
-   )
-   return http_response
 
 
 
