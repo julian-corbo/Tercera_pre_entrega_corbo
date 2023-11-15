aa2a7c4c6b72ba958b43008a7c364460e6cd4155
Julian C <juliancorbo00@gmail.com>
2023-11-15 12:51:58 -0300

    Unit Test para cafeterias

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
 
-# Create your tests here.
+from blog.models import Cafeterias
+
+
+class CafeteriasTests(TestCase):
+   """En esta clase van todas las pruebas del modelo Cafeterias."""
+
+   def test_creacion_cafeteria(self):
+       # caso uso esperado
+       cafeteria = Cafeterias(nombre="Culto", direccion="Bartolomé Mitre 1270")
+       cafeteria.save()
+
+       # Compruebo que el curso fue creado y la data fue guardad correctamente
+       self.assertEqual(Cafeterias.objects.count(), 1)
+       self.assertEqual(cafeteria.nombre, "Culto")
+       self.assertEqual(cafeteria.direccion, "Bartolomé Mitre 1270")
+
+
