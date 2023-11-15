916f1eba13ad964b3ea3563fef0625a19e45e481
Julian C <juliancorbo00@gmail.com>
2023-11-15 17:07:38 -0300

    Inicio modificado para mostrar articulos

--- entrega_corbo/views.py
+++ entrega_corbo/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from blog.models import Articulos
 
 def inicio(request):
-    contexto= {}
+    articulos = Articulos.objects.all()[:3]
+
+    contexto= {"articulos":articulos}
+
     http_response = render(
         request=request,
         template_name='inicio.html',
